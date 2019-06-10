1a7b29e7d474d79313f53ef4f7834acf07f7bc62
GekidouX <47258390+theclegginator@users.noreply.github.com>
2019-06-10 18:09:12 -0400

    Created database SQL files for dev

---
+++ models/question-seeds.sql
@@ -0,0 +1,3 @@
+USE flash_codes;
+
+INSERT INTO cards (category, question, answer) VALUES ('HTML/CSS', 'What does HTML stand for? What does CSS stand for?', 'Hypertext Markup Language /n Cascading Style Sheets');
