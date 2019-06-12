60e5f0e4d6f3f96c483b96d78c4e8a374a42c8c8
GekidouX <47258390+theclegginator@users.noreply.github.com>
2019-06-12 19:07:38 -0400

    Modified Handlebars and got API POST Route working. Handlebars form works with the API route so the index.handlbars file should be modified to how we want it to look

--- public/js/index.js
+++ public/js/index.js
@@ -1,99 +1,50 @@
 // Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
-var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
+const $categoryText = $("#category-menu");
+const $questionText = $("#question-text");
+const $answerDescription = $("#answer-description");
+const $submitBtn = $("#submit");
 
-// The API object contains methods for each kind of request we'll make
-var API = {
-  saveExample: function(example) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/examples",
-      data: JSON.stringify(example)
-    });
-  },
-  getExamples: function() {
-    return $.ajax({
-      url: "api/examples",
-      type: "GET"
-    });
-  },
-  // deleteExample: function(id) {
-  //   return $.ajax({
-  //     url: "api/examples/" + id,
-  //     type: "DELETE"
-  //   });
-  // }
-};
-
-// refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
-
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": example.id
-        })
-        .append($a);
-
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
-
-      $li.append($button);
-
-      return $li;
-    });
-
-    $exampleList.empty();
-    $exampleList.append($examples);
-  });
-};
-
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
+// Submitting new cards to API POST route.
+$("#submit").on("click", function() {
+  // clean up the values from the question and answer fields
+  let category = $categoryText.val().trim();
+  let question = $questionText.val().trim(); 
+  let answer = $answerDescription.val().trim();
   event.preventDefault();
-
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
-  };
-
-  if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
-    return;
+  if (checkForm(category, question, answer)) {
+    // build a new card object for uploading to the DB
+    let card = {
+      category: category,
+      question: question,
+      answer: answer
+    }
+    $.ajax("/api/cards", {
+        type: "POST",
+        data: card
+      }).then(function () {
+            console.log("Successfully added new card to DB.");
+            // clear out the text from the entry fields and return dropdown to default value.
+            $questionText.val("");
+            $answerDescription.val("");
+            $categoryText.val("default");
+      });
   }
+  // if the form wasn't properly filled out, show the user a modal.
+  else {
+    $("#formModal").modal();
+  }
+})
 
-  API.saveExample(example).then(function() {
-    refreshExamples();
-  });
-
-  $exampleText.val("");
-  $exampleDescription.val("");
-};
-
-// handleDeleteBtnClick is called when an example's delete button is clicked
-// Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
-
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
-  });
-};
-
-// Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
+function checkForm(category, question, answer) {
+    let passed = true;
+    if (category === null || category === undefined || category === "" || category === "default") {
+      passed = false;
+    }
+    if (question === null || question === undefined || question === "") {
+      passed = false;
+    }
+    if (answer === null || answer === undefined || answer === "") {
+      passed = false;
+    }
+    return passed; // send a true or false based on whether or not the form was filled out properly.
+}
