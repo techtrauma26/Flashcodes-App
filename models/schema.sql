0fa78e1d710aeebc4e40c7ba88d0e322acb4dcc0
GekidouX <47258390+theclegginator@users.noreply.github.com>
2019-06-13 18:23:51 -0400

    added author route and author category to SQL files

--- models/schema.sql
+++ models/schema.sql
@@ -1,13 +1,2 @@
 DROP DATABASE IF EXISTS flash_codes;
 CREATE DATABASE flash_codes;
- 
--- USE flash_codes;
-
--- CREATE TABLE cards
--- (
--- 	card_id INT NOT NULL AUTO_INCREMENT,
--- 	category VARCHAR(255) NOT NULL,
---     question VARCHAR(255) NOT NULL,
---     answer TEXT(500) NOT NULL,
--- 	PRIMARY KEY (card_id)
--- );
