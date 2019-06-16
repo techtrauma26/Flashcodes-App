09a7f37703f4bcc0c1bf22d77d2dec8fa50bdd93
L-Train <train19@gmail.com>
2019-06-16 01:27:54 -0400

    Updates

--- models/question-seeds.sql
+++ models/question-seeds.sql
@@ -1,3 +1,5 @@
+USE flash_codes;
+
 INSERT INTO Cards (author, category, question, answer) VALUES ('Cody', 'HTML', 'What does HTML stand for? What does CSS stand for?', 'Hypertext Markup Language');
 INSERT INTO Cards (author, category, question, answer) VALUES ('Cody', 'HTML', 'What is the main difference between a class and an ID in HTML?', 'A class can be applied to multiple elements while an ID can only be applied to one element.');
 INSERT INTO Cards (author, category, question, answer) VALUES ('Cody', 'HTML', 'What does CSS stand for?', 'Cascading Style Sheets');
